1a52503c1ae6c2f4e01d72b1015dde7414619d66
Prasad Patil <prpatil42@gmail.com>
2013-08-05 11:43:28 -0400

    Added limma, FDR calc, wrapper to run analysis

--- inst/butte/butte_helper.R
+++ inst/butte/butte_helper.R
@@ -1,49 +1,109 @@
+# Get annotation.
+# This function is called when it is not clear within a dataset which
+# samples are cases and which are controls. The user is prompted to proivde
+# an ordered vector indicating case/control in whataver format they choose.
+
+get_annotation <- function(pd){
+	cat("Here is the sample information for your dataset:\n")
+	print(pd)
+	cat("\n")
+	
+	ans <- readline(prompt=cat("Please provide a vector indiciating which samples are cases \nand which are controls,e.g. c('case','control','case')\n"))
+	x <- eval(parse(text=ans))
+
+	x
+}
+
+
 # Get gene list.
 # This function fetches GDS datasets from GEO for the IDs specified.
-# It then runs Significance Analysis of Microarrays (SAM) on each
-# dataset and returns a list of up- or down-regulated genes.
-
-# NOTE: To select the delta threshold for choosing cutoffs, we choose
-# the delta associated with the lowest estimated FDR. We also throw out
-# any genes without gene IDs.
+# It then runs a straightforward linear model via limma that calculates
+# the mean effect of case vs. control and uses the contrast to
+# generate a list of significant genes. We report the genes that have
+# non-empty gene IDs and p-values < 0.05 at an FDR of < 0.05. We also
+# have a prompt system in place to ask the user to properly annotate the
+# microrarray.
 
 get_gene_list <- function(id){
 
 	test_id <- paste("GDS", id, sep="")
 	test_gds <- getGEO(test_id, AnnotGPL=T)
 	test_eset <- GDS2eSet(test_gds)
-	# The gene names are in fData(test_eset)[,3]
-	#gene_ids <- fData(test_eset)[,c(1,3)]
 	gene_ids <- toupper(as.vector(fData(test_eset)[,3]))
 	# Rank normalize the data
 	test_eset <- normalize.ExpressionSet.quantiles(test_eset)
-	# Optional: get actual expression data
-	#eset_out <- exprs(test_eset)
 
-	# Run SAM
-	sam_out <- sam(test_eset, pData(test_eset)[,2], method="wilc.stat")
+	# Limma analysis. We're keeping it simple:
+	# case and control groups with a direct contrast.
 
-	# Here, need to make sam_out@chip <- ""
-	sam_out@chip <- ""
-	# I'm taking the gene list provided by the Delta with the lowest
-	# FDR that still produces significant genes...we should look into
-	# this, since the FDR is not always 0.05
-	fdr <- as.numeric(show(sam_out)$FDR)
-	sum <- summary(sam_out, show(sam_out)$Delta[which.min(fdr[fdr>0])])
+	pd <- pData(test_eset)
 
-	cutlow <- sum@mat.fdr[6]
-	#cutup <- sum@mat.fdr[7]
-	d.val <- sum@mat.sig$d.value
-	reg <- ifelse(d.val <= cutlow, "down", "up")
-	
-	# This gets the rows of the up- and down-regulated genes
-	rows <- sum@mat.sig$Row
-	#output <- cbind(gene_ids[rows,],"fold"=sam_out@fold[rows])
-	output <- data.frame("gene"=gene_ids[rows],"fold"=sum@mat.sig$R.fold,"reg"=reg)
-	# I'm throwing out any genes without gene names
-	idx <- which(output$gene == "")
-	output[-idx,]
-	
+	if(length(levels(pd[,2])) != 2 & length(levels(pd[,3])) != 2){
+		f <- get_annotation(pd)
+	} else if(length(levels(pd[,2])) == 2 & length(levels(pd[,3])) != 2){
+		ans <- ""
+		while(!(ans %in% c("y","n"))){
+			ans <- readline(prompt=paste("Using", colnames(pd)[2], "for annotation. OK? (y/n)",sep=" "))
+			ans <- tolower(substr(ans,1,1))
+		}
+
+		if(ans == "y"){
+			f <- pd[,2]
+		} else {
+			f <- get_annotation(pd)
+		}
+	} else if(length(levels(pd[,2])) != 2 & length(levels(pd[,3])) == 2){
+		ans <- ""
+		while(!(ans %in% c("y","n"))){
+			ans <- readline(prompt=paste("Using", colnames(pd)[3], "for annotation. OK? (y/n)",sep=" "))
+			ans <- tolower(substr(ans,1,1))
+		}
+
+		if(ans == "y"){
+			f <- pd[,3]
+		} else {
+			f <- get_annotation(pd)
+		}
+
+	} else {
+		ans <- ""
+		while(!(ans %in% c(2,3))){
+			ans <- readline(prompt=paste("Should I use 2:", colnames(pd)[2], "or 3:", colnames(pd)[3], " for annotation? (2/3) ",sep=" "))
+		}	
+		ans <- as.numeric(ans)
+		f <- pd[,ans]
+	}
+
+	f <- factor(gsub(" ","", f))
+	f <- factor(gsub("[[:punct:]]","",f))
+
+	design <- model.matrix(~0+f)
+	colnames(design) <- levels(f)
+
+	cat("I'm here 1\n")
+
+	fit <- lmFit(test_eset, design)
+
+	cat("I'm here 2\n")
+	ctrst <- as.character(paste(levels(f), collapse="-"))
+	cat("ctrst is", ctrst, "with class", class(ctrst), "\n")
+	cont.matrix <- makeContrasts(cc=ctrst,levels=design)
+
+	cat("I'm here 3\n")
+	fit2 <- contrasts.fit(fit,cont.matrix)
+	fit2 <- eBayes(fit2)
+
+	cat("I'm here 4\n")
+	tt <- topTable(fit2,number=100000)
+	tt <- tt[which(tt$adj.P.Val < 0.05),] # This is FDR!
+	output <- tt[,c("Gene.symbol", "logFC")]
+	names(output) <- c("gene", "fold")
+	reg <- ifelse(output$fold < 0, "down", "up")
+	output <- cbind(output, reg)
+	# throw out entries with no gene names
+	output <- output[-which(output$gene == ""),]
+	output$gene <- toupper(output$gene)
+	output
 }
 
 # Calculate the enrichment score.
@@ -52,6 +112,7 @@ get_gene_list <- function(id){
 # to calculate an enrichment score for the pair.
 # This function is called by calc_dds, which uses the enrichment score
 # to calculate an overall drug disease score.
+
 calc_es <- function(drug,subgene){
 	ord <- order(subgene$fold)
 	ord_drug <- drug[order(drug)]
@@ -59,8 +120,10 @@ calc_es <- function(drug,subgene){
 	v_j <- match(dis_gene, names(ord_drug))
 	# Tossing NAs for now
 	idx <- which(is.na(v_j))
-	v_j <- v_j[-idx]
-	dis_gene <- dis_gene[-idx]
+	if(length(idx) > 0){
+		v_j <- v_j[-idx]
+		dis_gene <- dis_gene[-idx]
+	}
 
 	a <- max((1:length(dis_gene))/length(dis_gene) - v_j/length(ord_drug))		
 	b <- max(v_j/length(ord_drug) - (0:(length(dis_gene)-1))/length(dis_gene))		
@@ -68,11 +131,13 @@ calc_es <- function(drug,subgene){
 	ifelse(a > b, a, -b)
 }
 
+
 # Calculate the drug disease score.
 # This function takes a drug signature and disease signature, then
 # splits the disease signature into up- and down-regulated gene sets.
 # The enrichment score is calculated for each set by calc_es(), and
 # the es_up and es_down scores are used to calculate the dds.
+
 calc_dds <- function(drug,dat){
 
 	uidx <- which(dat$reg == "up")
@@ -94,10 +159,13 @@ calc_dds <- function(drug,dat){
 # I guess we're assuming the null dist is symmetric...
 # This definitely needs to be changed somehow...need to use Levene's Test...
 
-calc_pval <- function(dat,dds){
+calc_pval <- function(dat,drugname,dds){
 	scores <- vector("numeric",100)
+	vec <- 1:length(drugname)
 	for(i in 1:100){
-		drug <- sample(1:length(dat),length(dat),replace=F)
+		idx <- sample(vec, dim(dat)[1], replace=F)
+		drug <- vec[idx]
+		names(drug) <- drugname[idx]
 		scores[i] <- calc_dds(drug,dat)
 	}
 	
---
+++ inst/butte/butte_runner.R
@@ -0,0 +1,9 @@
+# Run the butte analysis
+# This function expects as input a vector of numeric IDs,
+# the type of dataset each ID refers to (currently, only GDS is allowed),
+# and the number of drugs to test against (just the first n drugs from the
+# connectivity map dataset).
+butte_runner <- function(ids, id_types="GDS", ndrug=10){
+	knit("butte_template.Rmd")
+	markdownToHTML("butte_template.md")	
+}
