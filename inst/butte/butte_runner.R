84093d48b32a2a42d2148da499392e9c647959c2
Prasad Patil <prpatil42@gmail.com>
2013-08-06 12:13:13 -0400

    Added file timestamp...MakeContrast error still outstanding

--- inst/butte/butte_runner.R
+++ inst/butte/butte_runner.R
@@ -5,5 +5,9 @@
 # connectivity map dataset).
 butte_runner <- function(ids, id_types="GDS", ndrug=10){
 	knit("butte_template.Rmd")
-	markdownToHTML("butte_template.md",output="butte_analysis.html")	
+	time <- format(Sys.time(), "%Y_%m_%d_%H%M%S")
+	fn <- paste("butte_analysis_",time,".html",sep="")
+	cat(paste("Converting to html file: ",fn,"\n",sep=""))
+	markdownToHTML("butte_template.md",output=fn)
+	system("rm butte_template.md")	
 }
